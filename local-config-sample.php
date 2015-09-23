172750ffd2c7a574448b23f99288883ab3bc0db0
hushwolf <austin@ideum.com>
2015-09-23 15:11:26 -0600

    uploaded clone files

---
+++ local-config-sample.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Development database credentials
+ */
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'localhost' );
+
+/**
+ * Development server URL
+ */
+define( 'WP_HOME', 'http://localhost:8080/' );
+define( 'WP_SITEURL', WP_HOME . 'wordpress/' );
+
+/**
+ * Debug mode
+ */
+// define( 'SAVEQUERIES', true );
+// define( 'WP_DEBUG', true );
---
+++ local-config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Development database credentials
+ */
+define( 'DB_NAME', 'ideum_next' );
+define( 'DB_USER', 'admin' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'localhost' );
+
+/**
+ * Development server URL
+ */
+define( 'WP_HOME', 'http://hushwolf-ideum-next.dev' );
+define( 'WP_SITEURL', WP_HOME . 'wordpress/' );
+
+/**
+ * Debug mode
+ */
+// define( 'SAVEQUERIES', true );
+// define( 'WP_DEBUG', true );
